ea6295b9fe041fe652eb608f226e2799d5fd53c7
dilan-flores <dilan.flores@epn.edu.ec>
2024-01-10 00:51:38 -0500

    Agregar archivos de proyecto.

---
+++ Xuzzle/Xuzzle/XuzzleTile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Xuzzle
+{
+    class XuzzleTile : ContentView
+    {
+        public XuzzleTile(int row, int col, ImageSource imageSource)
+        {
+            Row = row;
+            Col = col;
+
+            Padding = new Thickness(1);
+            Content = new Image
+            {
+                Source = imageSource
+            };
+        }
+
+        public int Row { set; get; }
+
+        public int Col { set; get; }
+    }
+}
